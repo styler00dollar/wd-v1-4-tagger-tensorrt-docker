b7cbdaded757a0cacedf443447be4d440e15e09b
sudo <51405565+styler00dollar@users.noreply.github.com>
2023-06-04 04:23:20 +0200

    adding threading to inference

--- tag.py
+++ tag.py
@@ -1,62 +1,34 @@
-import onnxruntime
-import argparse
-import os
-import numpy as np
-import pandas as pd
-import cv2
 from tqdm import tqdm
+import argparse
+import concurrent.futures
+import cv2
+import numpy as np
+import onnxruntime
+import os
+import pandas as pd
 
 os.environ["ORT_TENSORRT_ENGINE_CACHE_ENABLE"] = "1"
 os.environ["ORT_TENSORRT_CACHE_PATH"] = "."
 
 # Define command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("--onnx", type=str, required=True, help="Path to ONNX model file")
-parser.add_argument(
-    "--input", type=str, required=True, help="Path to folder containing input images"
-)
-parser.add_argument(
-    "--tag_mapping", type=str, required=True, help="Path to tag mapping file"
-)
+parser = argparse.ArgumentParser(description="Image inference using ONNX model")
+parser.add_argument("--onnx", type=str, help="Path to ONNX model file")
+parser.add_argument("--input", type=str, help="Path to folder containing input images")
+parser.add_argument("--tag_mapping", type=str, help="Path to tag mapping csv file")
 parser.add_argument(
     "--threshold", type=float, default=0.5, help="Threshold for displaying tags"
 )
 parser.add_argument(
     "--corrupt", type=str, default="corrupt", help="Path to corrupt directory"
 )
-
-# Parse command line arguments
+parser.add_argument("--num_threads", type=int, default=1, help="Amount of Threads")
+parser.add_argument(
+    "--batch_size",
+    type=int,
+    default=1,
+    help="Batch size for image processing (currently only 1 supported)",
+)
 args = parser.parse_args()
-
-# Load ONNX model
-sess_options = onnxruntime.SessionOptions()
-providers = onnxruntime.get_available_providers()
-print(providers)
-sess_options.graph_optimization_level = (
-    onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
-)
-session = onnxruntime.InferenceSession(
-    args.onnx,
-    sess_options,
-    providers=[
-        "TensorrtExecutionProvider",
-        "CUDAExecutionProvider",
-        "CPUExecutionProvider",
-    ],
-)
-
-# Check if the --corrupt argument is specified
-if args.corrupt == "corrupt":
-    # Set a default location for the corrupt folder
-    default_corrupt_location = os.path.join(os.getcwd(), "corrupt")
-    args.corrupt = default_corrupt_location
-
-# Create the corrupt folder if it doesn't exist
-if not os.path.exists(args.corrupt):
-    os.makedirs(args.corrupt)
-
-
-# https://github.com/SmilingWolf/SW-CV-ModelZoo/blob/main/Utils/dbimutils.py
 
 
 def make_square(img, target_size):
@@ -83,43 +55,81 @@ def smart_resize(img, size):
     return img.astype(np.float32)
 
 
-# Load images from input folder and process them
-for image_file in tqdm(os.listdir(args.input)):
-    if (
-        image_file.endswith(".jpg")
-        or image_file.endswith(".jpeg")
-        or image_file.endswith(".png")
-    ):
-        image_path = os.path.join(args.input, image_file)
-
-        try:
-            img = cv2.imread(image_path)
-            if img is None:
-                # Move the unreadable/corrupt image file to a different folder
-                os.rename(image_path, os.path.join(args.corrupt, image_file))
-                continue
-        except Exception as e:
-            # Move the unreadable/corrupt image file to a different folder
+def process_image(image_path, session, tag_df, threshold):
+    try:
+        img = cv2.imread(image_path)
+        if img is None:
             os.rename(image_path, os.path.join(args.corrupt, image_file))
-            continue
+            return
+    except Exception as e:
+        os.rename(image_path, os.path.join(args.corrupt, image_file))
+        return
 
-        # Preprocess image
-        img = make_square(img, target_size=448)
-        img = smart_resize(img, size=448)
-        img = np.expand_dims(img, axis=0)
+    img = make_square(img, target_size=448)
+    img = smart_resize(img, size=448)
+    img = np.expand_dims(img, axis=0)
 
-        # Apply model and extract tags
-        tag_df = pd.read_csv(args.tag_mapping, dtype={"tag_id": int})
-        probabilities = session.run(None, {"input_1:0": img})[0][0]
-        tag_indices = np.argsort(probabilities)[::-1]
-        tag_df["probs"] = probabilities
-        found_tags = tag_df[tag_df["probs"] > args.threshold][["name"]]
+    probabilities = session.run(None, {"input_1:0": img})[0][0]
+    tag_indices = np.argsort(probabilities)[::-1]
+    tag_df["probs"] = probabilities
+    found_tags = tag_df[tag_df["probs"] > threshold][["name"]]
+    tag_names_str = ", ".join(found_tags["name"].tolist())
 
-        # Print results
-        tag_names_str = ", ".join(found_tags["name"].tolist())
+    txt_filename = os.path.splitext(os.path.basename(image_path))[0] + ".txt"
+    txt_path = os.path.join(os.path.dirname(image_path), txt_filename)
 
-        # Save tag string as file in the same directory as the image file
-        txt_filename = os.path.splitext(image_file)[0] + ".txt"
-        txt_path = os.path.join(os.path.dirname(image_path), txt_filename)
-        with open(txt_path, "w") as f:
-            f.write(tag_names_str)
+    with open(txt_path, "w") as f:
+        f.write(tag_names_str)
+
+
+def process_images(images, session, tag_df, threshold):
+    for image_file in images:
+        if (
+            image_file.endswith(".jpg")
+            or image_file.endswith(".jpeg")
+            or image_file.endswith(".png")
+        ):
+            image_path = os.path.join(args.input, image_file)
+            process_image(image_path, session, tag_df, threshold)
+
+
+sess_options = onnxruntime.SessionOptions()
+providers = onnxruntime.get_available_providers()
+print(providers)
+sess_options.graph_optimization_level = (
+    onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
+)
+
+sessions = []
+for i in range(args.num_threads):
+    session = onnxruntime.InferenceSession(
+        args.onnx,
+        sess_options,
+        providers=[
+            "TensorrtExecutionProvider",
+        ],
+    )
+    sessions.append(session)
+
+tag_df = pd.read_csv(args.tag_mapping, dtype={"tag_id": int})
+
+futures = []
+with concurrent.futures.ThreadPoolExecutor(max_workers=args.num_threads) as executor:
+    pbar = tqdm(total=len(os.listdir(args.input)), position=0, leave=True)
+    for i in range(0, len(os.listdir(args.input)), args.batch_size):
+        image_batch = os.listdir(args.input)[i : i + args.batch_size]
+        future = executor.submit(
+            process_images,
+            image_batch,
+            sessions[i % args.num_threads],
+            tag_df,
+            args.threshold,
+        )
+        future.add_done_callback(lambda p: pbar.update(1))
+        futures.append(future)
+    # Use a dummy loop to ensure the bar stays active
+    for _ in tqdm(
+        concurrent.futures.as_completed(futures), total=len(futures), leave=False
+    ):
+        pass
+    pbar.close()
